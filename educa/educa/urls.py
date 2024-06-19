0ffcf995496955ef2e3ef8064a2dff7620a4339f
danaendraraj05 <danaendraraj@testpress.in>
2024-06-19 10:48:49 +0530

    Added Channels

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     path('students/', include('students.urls')),
     path('', CourseListView.as_view(), name='course_list'),
+    path('chat/', include('chat.urls', namespace='chat')),
     path('api/', include('courses.api.urls', namespace='api')),
 ]
 
